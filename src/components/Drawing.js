2059545787de67907a0376e88d02befbdafe4a11
Jimmy Lin <jimmy@MacBook-Pro.local>
2020-06-05 11:18:42 -0700

    put logic code into different functions

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -7,52 +7,40 @@ const Drawing = ({ saveDoc }) => {
   const viewer = useRef(null);
   const currentUser = window.location.pathname.slice(1);
 
-  const createTimestampAnnotation = Annotations => {
-    // error to get page width
-    // console.log('page width', CoreControls.DocumentViewer.getPageWidth(1));
-    // try to get page width for place annotation to the bottom right corner
-    // const iframe = document.getElementById('webviewer-1');
-    // const currentDoc = iframe.contentWindow.document.querySelector(
-    //   '#pageContainer0'
-    // );
-    // const docWidth = currentDoc.offsetWidth;
-    // addAnnotation(annotManager, freeText);
-    // annotManager.redrawAnnotation(freeText);
-    const freeText = new Annotations.FreeTextAnnotation();
-    freeText.PageNumber = 1;
-    console.log(freeText.getBottom());
-
-    freeText.X = 400;
-    freeText.Y = 350;
-    freeText.Width = 200;
-    freeText.Height = 20;
-    freeText.setPadding(new Annotations.Rect(0, 0, 0, 0));
-    // content is the time stamp
-    const date = new Date();
-    // timestamp in YYYY-M-D hh:mm:ss format
-    const timestamp = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    freeText.setContents(`last action: ${timestamp}`);
-    freeText.FillColor = new Annotations.Color(0, 255, 255);
-    freeText.FontSize = '14pt';
-    return freeText;
-  };
-
   // if using a class, equivalent of componentDidMount
   useEffect(() => {
-    WebViewer(
-      {
-        path: '/webviewer/lib',
-        initialDoc: 'https://i.imgur.com/u2oPC.jpg',
-        showLocalFilePicker: true,
-        fullAPI: true,
-      },
-      viewer.current
-    ).then(instance => {
-      // instance.loadDocument(
-      //   'blob:http://localhost:3000/60016a4f-2684-4668-b28e-2159683da53b',
-      //   { filename: 'myfile.pdf' }
+    const createTimestampAnnotation = Annotations => {
+      // error to get page width
+      // console.log('page width', CoreControls.DocumentViewer.getPageWidth(1));
+      // try to get page width for place annotation to the bottom right corner
+      // const iframe = document.getElementById('webviewer-1');
+      // const currentDoc = iframe.contentWindow.document.querySelector(
+      //   '#pageContainer0'
       // );
-      const { Annotations, annotManager, docViewer } = instance;
+      // const docWidth = currentDoc.offsetWidth;
+      // addAnnotation(annotManager, freeText);
+      // annotManager.redrawAnnotation(freeText);
+      const freeText = new Annotations.FreeTextAnnotation();
+      freeText.PageNumber = 1;
+      console.log(freeText.getBottom());
+
+      freeText.X = 400;
+      freeText.Y = 350;
+      freeText.Width = 200;
+      freeText.Height = 20;
+      freeText.setPadding(new Annotations.Rect(0, 0, 0, 0));
+      // content is the time stamp
+      const date = new Date();
+      // timestamp in YYYY-M-D hh:mm:ss format
+      const timestamp = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+      freeText.setContents(`last action: ${timestamp}`);
+      freeText.FillColor = new Annotations.Color(0, 255, 255);
+      freeText.FontSize = '14pt';
+      return freeText;
+    };
+
+    const handleSaveDoc = instance => {
+      const { annotManager, docViewer } = instance;
       // Add header button that will get file data on click
       instance.setHeaderItems(header => {
         header.push({
@@ -74,16 +62,12 @@ const Drawing = ({ saveDoc }) => {
           },
         });
       });
-      annotManager.setCurrentUser(currentUser);
-      // disable all tools and only enable free hand tool
-      instance.disableTools();
-      instance.enableTools(['AnnotationCreateFreeHand']);
+    };
 
-      annotManager.disableFreeTextEditing();
+    const handleAnnotChanged = (Annotations, annotManager) => {
       annotManager.on('annotationChanged', (annotations, action, event) => {
         if (event.imported) return;
-        // do event handling
-        console.log('ANNOTATION CHANGED');
+        // watch AnnotationCreateFreeHand
         if (annotations[0].ToolName === 'AnnotationCreateFreeHand') {
           // find previous timestamp annotation
           const annots = annotManager.getAnnotationsList();
@@ -98,9 +82,34 @@ const Drawing = ({ saveDoc }) => {
           const timestampAnnot = createTimestampAnnotation(Annotations);
           annotManager.addAnnotation(timestampAnnot);
           annotManager.redrawAnnotation(timestampAnnot);
-          docViewer.on('documentLoaded', () => {});
         }
       });
+    };
+
+    WebViewer(
+      {
+        path: '/webviewer/lib',
+        initialDoc: 'https://i.imgur.com/u2oPC.jpg',
+        showLocalFilePicker: true,
+        fullAPI: true,
+      },
+      viewer.current
+    ).then(instance => {
+      // instance.loadDocument(
+      //   'blob:http://localhost:3000/60016a4f-2684-4668-b28e-2159683da53b',
+      //   { filename: 'myfile.pdf' }
+      // );
+      const { Annotations, annotManager } = instance;
+
+      handleSaveDoc(instance);
+      annotManager.setCurrentUser(currentUser);
+
+      // disable all tools and only enable free hand tool
+      instance.disableTools();
+      instance.enableTools(['AnnotationCreateFreeHand']);
+
+      annotManager.disableFreeTextEditing();
+      handleAnnotChanged(Annotations, annotManager);
     });
   }, [currentUser, saveDoc]);
 
